63c77e021fa1598ed3124222c82d4f1d71ed0e91
Mayur verma <vermaxmayur100@gmail.com>
2023-01-26 19:11:49 +0530

    initial commit

--- .vscode/practice.cpp
+++ .vscode/practice.cpp
@@ -13,50 +13,66 @@ int binarySearch(int arr[], int n, int k)
 
         if (k < arr[mid])
         {
-            end = mid;
-            mid = (start + end) / 2;
+            end = mid - 1;
         }
         else if (k > arr[mid])
         {
-            start = mid;
-            mid = (start + end) / 2;
+            start = mid + 1;
         }
     }
 }
-int recursiveBs(int arr[], int n, int k,int mid,int start,int end)
-{
-    if (k == arr[mid])
-        return mid;
-    
-    return if()
-}
-int twoPointers(int arr[], int n, int k)
-{
+// int recursiveBs(int arr[], int n, int k,int mid,int start,int end)
+// {
+//     if (k == arr[mid])
+//         return mid;
 
-}
-int squareRoot(int n)
-{
+//     return if()
+// }
+// int twoPointers(int arr[], int n, int k)
+// {
 
-}
-int searchInRotated(int arr[], int n, int k)
-{
+// }
+// int squareRoot(int n)
+// {
 
-}
-int count1s(bool arr[])
-{
+// }
+// int searchInRotated(int arr[], int n, int k)
+// {
 
-}
-int findPeak(int arr[])
-{
-    
-}
+// }
+// int count1s(bool arr[], int n) // if sorted array
+// {
+//     int start = 0;
+//     int end = n-1;
+//     int mid;
+//     while(arr[start]<arr[end])
+//     {
+//         mid = (start+end)/2;
+//         if(arr[mid]==1)
+//         {
+//             end = mid-1;
+//         }
+//         else
+//         {
+//             start = mid+1;
+//         }
+//     }
+// }
+// int findPeak(int arr[])
+// {
+
+// }
+// bool prac(int x, int y)
+// {
+//     return ((x < y) ? x = y : 1);
+// }
 int main()
 {
-    
-    int start = 0;
-    int end = 6;
-    int mid = (start+end)/2;
+
+    // int start = 0;
+    // int end = 6;
+    // int mid = (start+end)/2;
     int arr[7] = {10, 20, 33, 43, 56, 64, 110};
-    cout << binarySearch(arr, 7, 10);
+    cout << binarySearch(arr, 7, 110);
     return 0;
 }
