7405006495151a672353b74f394a356a823cef65
Mayur verma <vermaxmayur100@gmail.com>
2023-02-13 21:57:30 +0530

    sdfsdfsf

--- .vscode/basic.cpp
+++ .vscode/basic.cpp
@@ -4,20 +4,22 @@ using namespace std;
 // 8 6
 // 4 5 6 1 2 3 8 7   1 2 3 4 5 6 8 7  1 2 3 4 5 6 7 8
 // 2
+// void anotSol(stirng s,)
 
-void solution(string s)
-{
-  int n = s.length();
-  for (int i = n-1; i >= 0; i--)
-  {
-    if(s[i]==' ')
-      cout<<s[i];
-  }
-}
+// void solution(string s)
+// {
+//   int n = s.length();
+//   for (int i = n-1; i >= 0; i--)
+//   {
+//     if(s[i]==' ')
+//       cout<<s[i];
+//   }
+// }
 int main()
 {
-  string s;
-  cin>>s;
-  solution(s);
+  cout<<"enter your name"<<endl;
+   string s;
+   getline(cin,s);
+   cout<<"Your name is : "<< s <<endl;
   return 0;
 }
